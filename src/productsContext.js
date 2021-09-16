79a5dfb47427717a28ac94f14507eb5d9952267c
hangnguyen81 <hangntt@gmail.com>
2021-09-16 12:59:14 +0300

    update Context, use localStorage to store data

--- src/productsContext.js
+++ src/productsContext.js
@@ -4,12 +4,24 @@ import productsData from "./productsData";
 const Context = React.createContext()
 
 const ContextProvider = ({children}) =>{
-    const [allProducts, setAllProducts] = useState([])
-    const [cartItems, setCartItems] = useState([])
+
+    const [allProducts, setAllProducts] = useState(() =>{
+        const productData = JSON.parse(localStorage.getItem('allProducts'))
+        return productData?productData:productsData
+    })
+        
+    const [cartItems, setCartItems] = useState(() =>{
+        const cartData = JSON.parse(localStorage.getItem('cartItems'))
+        return cartData?cartData:[]
+    })
+    
+    useEffect(() =>{
+        localStorage.setItem('allProducts', JSON.stringify(allProducts))
+    },[allProducts])
 
     useEffect(() =>{
-        setAllProducts(productsData)
-    },[])
+        localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    },[cartItems])
 
     const toggleFavour = (id) =>{
         const updateProducts = allProducts.map(product =>{
@@ -22,20 +34,24 @@ const ContextProvider = ({children}) =>{
                 return product
         })
         setAllProducts(updateProducts)
+
     }
 
     const addToCart = (product) =>{
         const updateCartItems = cartItems.concat(product)
         setCartItems(updateCartItems)
+        
     }
 
     const removeFromCart = (id) =>{
         const updateCartItems = cartItems.filter(item => item.id !==id)
         setCartItems(updateCartItems)
+
     }
 
     const emptyCart = () =>{
         setCartItems([])
+
     }
     return(
         <Context.Provider value={{
